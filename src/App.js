0e24418e43de197f9d30d82abcce4253e38bab9c
Siddharth Singh <siddharthsingh1552@gmail.com>
2023-07-13 12:29:51 +0530

    Added Comments in app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import './App.css';
-import searchicon from './search.svg';
-import MovieCard from './MovieCard';
-import MovieDetails from './MovieDetails';
-import React, { useEffect, useState } from 'react';
-import moviesData from './movies.json';
-import DayNightCycle from './DayNightCycle';
-import Footer from './Footer';
+import './App.css'; // Importing CSS file for styling
+import searchicon from './search.svg'; // Importing search icon image
+import MovieCard from './MovieCard'; // Importing MovieCard component
+import MovieDetails from './MovieDetails'; // Importing MovieDetails component
+import React, { useEffect, useState } from 'react'; // Importing necessary React components and hooks
+import moviesData from './movies.json'; // Importing movie data from a JSON file
+import DayNightCycle from './DayNightCycle'; // Importing DayNightCycle component
+import Footer from './Footer'; // Importing Footer component
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [selectedMovie, setSelectedMovie] = useState(null);
-  const [darkMode, setDarkMode] = useState(false);
+  const [movies, setMovies] = useState([]); // State variable to store movies data
+  const [searchTerm, setSearchTerm] = useState(''); // State variable to store the search term
+  const [selectedMovie, setSelectedMovie] = useState(null); // State variable to store the selected movie
+  const [darkMode, setDarkMode] = useState(false); // State variable to store the dark mode state
 
   const findMovies = (title) => {
+    // Function to filter movies based on the search term
     const filteredMovies = moviesData.filter((movie) =>
       movie.Title.toLowerCase().includes(title.toLowerCase())
     );
@@ -21,29 +22,35 @@ function App() {
   };
 
   const handleSearch = () => {
+    // Function to handle the search action
     findMovies(searchTerm);
   };
 
   const handleKeyDown = (e) => {
+    // Function to handle keydown event for search
     if (e.key === 'Enter') {
       handleSearch();
     }
   };
 
   const handleMovieClick = (movie) => {
+    // Function to handle the click on a movie card
     setSelectedMovie(movie);
   };
 
   const handleModeToggle = () => {
+    // Function to toggle between dark mode and light mode
     setDarkMode(!darkMode);
   };
 
   useEffect(() => {
+    // useEffect hook to initialize the movie list and apply dark mode changes
     findMovies('');
     document.body.classList.toggle('dark-mode', darkMode);
   }, [darkMode]);
 
   useEffect(() => {
+    // useEffect hook to apply background color based on dark mode state
     if (darkMode) {
       document.body.style.backgroundColor = '#212426';
     } else {
